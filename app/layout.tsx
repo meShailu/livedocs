b70b3ee21d9ab2918dd32c5f0dde285ac25fde4b
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-20 23:54:41 +0200

    button component installed from shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
-import "@/styles/globals.css";
 import { Inter as FontSans } from "next/font/google";
-
+import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Metadata } from "next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+  title: "LiveDoce",
+  description: "Your go-to collaborative editor",
+};
 
 export default function RootLayout({
   children,
@@ -15,14 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
+        className={cn("min-h-screen font-sans antialiased", fontSans.variable)}
       >
-        ...
+        {children}
       </body>
     </html>
   );
